bf6d6e0d1f7fc064fc15aadafeac3e7b8f9cab40
Tanu Shree <81132025+m-lil-coder@users.noreply.github.com>
2021-03-24 21:19:31 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,11 @@
+
+
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello I am Tanu\n");
+   printf("******************************************************************************\n");
+   printf("this is my first website and i happy to launch it. ");
+    return 0;
+}
